7f7884e9e274ebd307f58c91c723ef61525c130f
ruku901 <rukunujjamans@gmail.com>
2026-09-06 21:06:38 +0600

    Dynamic rendering map,sort,filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
-import './App.css'
-import Card from './components/Card'
-import TestContent  from './TestContent'
+import "./App.css";
+import Card from "./components/Card";
+// import TestContent from "./TestContent";
 
 function App() {
+  const users = [
+    { name: "Rakib", age: 88 },
+    { name: "Raza", age: 80 },
+    { name: "Mizan", age: 13 },
+    { name: "Zeshan", age: 17 },
+  ];
 
   return (
     <>
-      <h1>Hello World</h1>
-      <TestContent/>
-      <Card firstName= "Rukunujjaman" lastName = "Sarker" age = {32}>
-        <h3>Learning Props</h3>
-        <p>It's a important concept</p>
-      </Card>
+      {/* <Card name = {users[0].name} age = {users[0].age} />
+    <Card name = {users[1].name} age = {users[1].age} />
+    <Card name = {users[2].name} age = {users[2].age} />
+    <Card name = {users[3].name} age = {users[3].age} /> */}
+
+      {users
+        .filter((u) => u.age > 18)
+        .sort((a, b) => a.age - b.age)
+        .map((u) => (
+          <Card name={u.name} age={u.age} />
+        ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,10 @@
-export default function Card({children, firstName, lastName, age}){
-    return (
-        <div>
-            <h2>Card Component</h2>
-            <p>My  name is {firstName} {lastName}</p>
-            <p>I'm {age} years old</p>
-            {children}
-        </div>
-    )
+import "./Card.css";
+
+export default function Card({ name, age }) {
+  return (
+    <div className="card">
+      <p>Name: {name}</p>
+      <p>Age: {age}</p>
+    </div>
+  );
 }
