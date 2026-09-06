f653ab36b533a9b7893eae61fc8741b77dbf72cc
ruku901 <rukunujjamans@gmail.com>
2026-09-06 22:00:45 +0600

    useState hook practice

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
 import "./Card.css";
 
 export default function Card({ name, age }) {
+  const [user, setUser] = useState("");
+
+  const clickHandler = (userName) => {
+    setUser(userName);
+  };
   return (
-    <div className="card">
+    <div className="card" onClick={() => clickHandler(name)}>
       <p>Name: {name}</p>
       <p>Age: {age}</p>
+      {user && <p>Greeting {user}</p>}
     </div>
   );
 }
