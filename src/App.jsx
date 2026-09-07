10a3a15c6726b19ae5cc2e9b58aa6626149ac6c1
ruku901 <rukunujjamans@gmail.com>
2026-09-07 20:49:57 +0600

    useState basic covered

--- src/App.jsx
+++ src/App.jsx
@@ -4,37 +4,48 @@ import Card from "./components/Card";
 import Counter from "./components/Counter";
 // import TestContent from "./TestContent";
 
-const users = [
-  { name: "Rakib", age: 88 },
-  { name: "Raza", age: 80 },
-  { name: "Mizan", age: 13 },
-  { name: "Zeshan", age: 17 },
-];
+// const users = [
+//   { name: "Rakib", age: 88 },
+//   { name: "Raza", age: 80 },
+//   { name: "Mizan", age: 13 },
+//   { name: "Zeshan", age: 17 },
+// ];
 function App() {
-
   const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [members, setMembers] = useState([
+    { name: "Rakib", age: 88 },
+    { name: "Raza", age: 80 },
+    { name: "Mizan", age: 13 },
+    { name: "Zeshan", age: 17 },
+  ]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(name);
-    
-  }
+    // setMembers([...members, { name, age }]);
+    setMembers((prev) => [...prev, {name, age}])
+    console.log(members);
+
+    setName("");
+    setAge("");
+  };
 
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <input type="text" onChange={(e) => setName(e.target.value)} />
+        <input type="text" onChange={(e) => setName(e.target.value)} value={name} />
+        <input type="number" onChange={(e) => setAge(e.target.value)} value={age} />
         <button type="submit">Submit</button>
       </form>
 
-      {users
+      {members
         .filter((u) => u.age > 18)
         .sort((a, b) => a.age - b.age)
         .map((u) => (
           <Card name={u.name} age={u.age} />
         ))}
 
-        <Counter />
+      <Counter />
     </>
   );
 }
